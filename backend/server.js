687abf74e5cfa2256fe21d99e79fee0e3ec32d35
Ayush <113465008+ayush1944@users.noreply.github.com>
2025-07-15 04:23:03 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-    origin: 'https://kabutar-beta.vercel.app',
+    origin: '*',
     methods: ['GET', 'POST'],
     credentials: true
   }
